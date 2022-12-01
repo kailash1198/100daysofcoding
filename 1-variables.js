e587907796275cb41f042b9fdf8ec5ce839f7a56
kailash <kailash414243@gmail.com>
2022-12-01 10:16:22 +0530

    day 1 of 100 days challange. Topics: Variables

--- 1-variables.js
+++ 1-variables.js
@@ -1,1 +1,23 @@
 // Variables in Javascript
+// let firstName = "My Name";
+// console.log(firstName);
+
+// firstName = "My Name 2"
+// console.log(firstName);
+
+//let firstName = "Your Name"; //Error, because you cannot redclare variable using let keywords
+
+// var numOne = 10;
+// console.log(numOne);
+
+// numOne = 20;
+// console.log(numOne);
+
+// var numOne = 50;
+// console.log(numOne);
+
+// const numOne = 50;
+// console.log(numOne);
+
+//numOne = 60; // error, because const is fixed you cannot redclare and as well as you cannot change the values of constants.
+// console.log(numOne);
