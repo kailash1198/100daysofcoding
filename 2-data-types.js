682839addd544670fccf0fd996046697cf421640
kailash <kailash414243@gmail.com>
2022-12-02 19:38:21 +0530

    day 2 challange done practice of data types

---
+++ 2-data-types.js
@@ -0,0 +1,41 @@
+// ------ DATA TYPES IN JAVASCRIPT ------
+
+
+// === Number ===== 
+// let myNum = 10;
+// console.log(myNum);
+// console.log(typeof myNum);
+
+// === String ===== 
+
+// let myStr = "Hello Kailash";
+// console.log(myStr);
+// console.log(typeof myStr);
+
+// let myStr2 = 'This is Text';
+// console.log(myStr2);
+// console.log(typeof myStr2);
+
+// ====== Boolean ======
+
+// const myBooleanValue = 10 < 12;
+// console.log(myBooleanValue);
+// console.log(typeof myBooleanValue);
+
+// ======= Null but return Object ====== 
+// let myData = null;
+// console.log(myData);
+// console.log(typeof myData);
+
+// ===== Undefined ======
+// let myData;
+// console.log(myData);
+// console.log(typeof myData);
+
+// ===== BigInt ======
+// let myBigNum = 198374862368746872364878236874628736487243;
+// console.log(myBigNum);
+// console.log(typeof myBigNum);
+// let newTypes = BigInt(myBigNum);
+// console.log(newTypes);
+// console.log(typeof newTypes);
