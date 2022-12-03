0e81351114078eeed00644b8a4f4c774e2beae9d
kailash <kailash414243@gmail.com>
2022-12-03 19:53:18 +0530

    type casting practice

---
+++ type-casting.js
@@ -0,0 +1,37 @@
+// ====== TYPE CASTING/CONVERSION =======
+
+// let myNum = 10;
+// console.log(typeof myNum);
+
+// let changeNumString = String(myNum);
+// console.log(typeof changeNumString);
+
+
+// let changeNumBoolean = Boolean(changeNumString);
+// console.log(typeof changeNumBoolean);
+
+// let changeBooleanNumber = Number(changeNumBoolean);
+// console.log(typeof changeBooleanNumber);
+
+// let a = 10;
+// let b = "10";
+
+// let c = a + b;
+// console.log(c);
+// console.log(typeof c);
+
+//c = a + +b; //add extra + operator
+// console.log(c);
+// console.log(typeof c);
+
+
+// let numOne = 20;
+// let numTwo = "10";
+
+// let minusNum = numOne - numTwo;
+// console.log(minusNum);
+// console.log(typeof minusNum);
+
+// minusNum = -numOne - -numTwo;
+// console.log(minusNum);
+// console.log(typeof minusNum);
